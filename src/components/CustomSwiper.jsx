d7ba8fcd948f360f6eb4575a5b011eb8a2537a46
loeybho <byo_l@naver.com>
2024-05-17 14:54:27 +0900

    Style. AboutMe에 Swiper 추가

---
+++ src/components/CustomSwiper.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+const Wrapper = styled.div`
+  .contents-swiper {
+    max-width: 100%;
+  }
+
+  img {
+    width: 100%;
+  }
+`;
+
+function CustomSwiper() {
+  return (
+    <Wrapper>
+      <div className="contents-swiper">
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          }}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={10}
+          slidesPerView={3}
+          navigation={true}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          centeredSlides={true}
+        >
+          <SwiperSlide>
+            <img src="/3.jpeg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/4.jpeg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/3.jpeg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/4.jpeg" alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default CustomSwiper;
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,10 +1,10 @@
+import CustomSwiper from "@components/CustomSwiper";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
   background-color: black;
   color: white;
   padding: 0 20px;
-  height: 100vh;
   padding-top: 20px;
   .all-wrapper {
     max-width: 800px;
@@ -29,11 +29,21 @@ const Wrapper = styled.div`
     }
   }
 
+  .all-contents {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    font-size: 1.8rem;
+    line-height: 1.285;
+    font-weight: 300;
+  }
+
+  .contents-header {
+    text-align: center;
+  }
+
   .contents {
     margin-top: 20px;
-    box-shadow: inset 0 0 20px red;
-    font-size: 1.6rem;
-    line-height: 1.285;
   }
 `;
 
@@ -43,16 +53,26 @@ function AboutMe() {
       <Wrapper>
         <div className="all-wrapper">
           <h1 className="category">About Me</h1>
-          <div className="contents">
-            <p>Connect the dots</p>
-            <h2>그래픽 디자인에서 프론트엔드 개발까지</h2>
-            <p>
-              그래픽 디자이너로 약 2년 간 근무했던 저는
-              <br />
-              고객의 클릭을 유도하는 이미지를 만들었습니다.
-              <br />
-              이에 흥미가 생겨 프론트엔드 개발을 배우기 시작했습니다.
-            </p>
+
+          <div className="all-contents">
+            <div className="contents-header">
+              <p>Connect the dots</p>
+              <h2>그래픽 디자인에서 프론트엔드 개발까지</h2>
+            </div>
+            <div className="contents">
+              <div>
+                <CustomSwiper />
+              </div>
+              <div>
+                <p>
+                  그래픽 디자이너로 약 2년 간 고객의 클릭을 유도하는 이미지를
+                  만들었습니다.
+                  <br />
+                  화면에 의도한 바를 구현하고, 사용자의 행동을 유도하는 것에
+                  흥미가 생겨 프론트엔드 개발을 배우기 시작했습니다.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Wrapper>
