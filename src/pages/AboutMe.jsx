b64268b2f2b5e4dac7b5d850175a4d98eda102c1
loeybho <byo_l@naver.com>
2024-05-23 11:35:05 +0900

    Style. 배경 블랙 수정

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
 
   h1 {
     font-weight: 700;
-    font-size: 2.2rem;
+    font-size: 4rem;
   }
 
   .category {
@@ -30,6 +30,13 @@ const Wrapper = styled.div`
     font-size: 1.8rem;
     line-height: 1.285;
     font-weight: 300;
+  }
+
+  .contents {
+    font-family: "NotoSans";
+    font-weight: 400;
+    line-height: 1.5;
+    letter-spacing: -0.4px;
   }
 
   .contents-header {
@@ -58,13 +65,16 @@ function AboutMe() {
               </div>
               <div>
                 <h2>그래픽 디자인에서 프론트엔드 개발까지</h2>
-                <p>
-                  그래픽 디자이너로 약 2년 간 고객과 상호작용하고, 클릭을
-                  사로잡는 콘텐츠를 만들었습니다.
-                  <br />
-                  의도한 바를 화면에 구현하고, 사용자의 행동을 유도하는 디자인에
-                  흥미가 생겨 UX, UI와 프론트엔드 개발을 배우기 시작했습니다.
-                </p>
+                <div className="contents">
+                  <p>
+                    그래픽 디자이너로 약 2년 간 고객과 상호작용하고, 클릭을
+                    사로잡는 콘텐츠를 만들었습니다.
+                    <br />
+                    의도한 바를 화면에 구현하고, 사용자의 행동을 유도하는
+                    디자인에 흥미가 생겨 UX, UI와 프론트엔드 개발을 배우기
+                    시작했습니다.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
